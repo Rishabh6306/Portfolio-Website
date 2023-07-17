c70833e3069b539c28b05f5ea71e393eeaf533fc
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-07-18 00:29:47 +0530

    Portfolio Website Created

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import  React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { FiMenu } from 'react-icons/fi';
 
 export default function Navbar() {
