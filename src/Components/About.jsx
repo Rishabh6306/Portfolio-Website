3a3c0375e38113dead2b73e0f6e0be6ca6c34a27
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-10-01 14:09:40 +0530

    fix spelling

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
             <div id="about-container">
                 <h2 id='heading'>About  <span>Me</span></h2>
                 <h3>Frontend Developer</h3>
-                <p>Rishabh Srivastava, a passionate web development enthusiast, currently pursuing a course to enhance my skills. As a fresher, I am eager to explore new opportunities and contribute my best to the world of web development. I'm committed to honing my abilities and making a positive impact in the industry. Excited for the journey ahead!
+                <p>Rishabh Srivastava, a passionate web developer enthusiast, currently pursuing a course to enhance my skills. As a fresher, I am eager to explore new opportunities and contribute my best to the world of web development. I'm committed to honing my abilities and making a positive impact in the industry. Excited for the journey ahead!
                 </p>
                 <a href='#' id='btn'>Read More</a>
             </div>
